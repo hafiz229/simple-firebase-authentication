07b29b27131ef0229d677cfcf9d3412a0980be96
Hafiz <hasnathafiz229>
2021-10-16 02:11:13 +0600

    Add Firebase to SDK

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqIWH2lZwFW6qcp0mTlh-3e7PkFXCHaDA",
+  authDomain: "simple-firebase-authenti-6b17a.firebaseapp.com",
+  projectId: "simple-firebase-authenti-6b17a",
+  storageBucket: "simple-firebase-authenti-6b17a.appspot.com",
+  messagingSenderId: "930599274352",
+  appId: "1:930599274352:web:5928d1b2a1c60260cc31b5",
+};
+
+export default firebaseConfig;
---
+++ src/Firebase/firebase.initialize.js
@@ -0,0 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+// Initialize Firebase inside a function
+const initializeAuthenticatiton = () => {
+  initializeApp(firebaseConfig);
+};
+export default initializeAuthenticatiton;
