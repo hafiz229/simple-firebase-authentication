6409445f3ab48b1238a51f02b75d7dd3579be2e1
Hafiz <hasnathafiz229>
2021-10-18 03:17:08 +0600

    Facebook Sign-In Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   GithubAuthProvider,
+  FacebookAuthProvider,
   signOut,
 } from "firebase/auth";
 import { useState } from "react";
@@ -13,6 +14,7 @@ initializeAuthentication();
 
 const googleProvider = new GoogleAuthProvider();
 const gitHubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 const auth = getAuth();
 
 function App() {
@@ -53,6 +55,23 @@ function App() {
       });
   };
 
+  // handle facebook sign in
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   // handle sign out for all logged in accounts
   const handleSignOut = () => {
     signOut(auth).then(() => {
@@ -66,6 +85,7 @@ function App() {
         <div>
           <button onClick={handleGoogleSignIn}>Google Sign-In</button>
           <button onClick={handleGitHubSignIn}>GitHub Sign-In</button>
+          <button onClick={handleFacebookSignIn}>Facebook Sign-In</button>
         </div>
       ) : (
         <button onClick={handleSignOut}>Sign Out</button>
